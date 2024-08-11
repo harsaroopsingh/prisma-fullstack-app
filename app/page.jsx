7a0baf7ba3c186b576fa2a0fddaf00e22ebe03ec
harsaroopsingh <harsaroop89@gmail.com>
2024-08-11 16:08:54 -0400

    show posts

---
+++ app/page.jsx
@@ -0,0 +1,37 @@
+import styles from "./page.module.css";
+import prisma from "../lib/prisma";
+import Post from "./components/posts";
+
+async function getPosts(){
+  const posts = await prisma.post.findMany({
+    where: {published: true},
+    include: {
+      author: {
+        select: {name: true}
+      }
+    }
+  });
+  return posts;
+}
+
+export default async function Home() {
+  const posts = await getPosts();
+  return (
+    <main className={styles.main}>
+      <h1>Feed</h1>
+      {
+        posts.map((post) => {
+          return (
+            <Post
+              key={post.id}
+              id={post.id}
+              title={post.title}
+              content={post.content}
+              authorName={post.authorName}
+            />
+          )
+        })
+      }
+    </main>
+  );
+}
